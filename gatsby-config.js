58773c450ff733d0acf203440d4e02dc88f0e8fa
Bote Wang <retr0p@outlook.com>
2021-08-31 16:46:02 +0800

    added file-source-system

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,21 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "T1m Wang",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `data`,
+        path: `${__dirname}/src/data/`,
+        ignore: [`**/\.*`], // ignore files starting with a dot
+      },
+    },
+  ],
 };
