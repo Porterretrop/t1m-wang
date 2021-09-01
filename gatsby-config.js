a06261a8d27c85f50d9c8967b0496dce44b41a69
Bote Wang <retr0p@outlook.com>
2021-09-01 15:55:14 +0800

    fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,28 +4,8 @@ module.exports = {
     title: "T1m Wang",
   },
   plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
-    },
-    {
-      resolve: `gatsby-sourcssssse-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      },
-    },
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        names:'pages',
-        path:'${_dirname}/src/pages/',
-      },
-    },
-      'gatsby-plugin-mdx',
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
   ],
 };
